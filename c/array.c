b8548b1f28cc2747e46321f7385059cd38cba730
Devadath krishna <118240648+devadathkrishna@users.noreply.github.com>
2022-11-15 10:08:21 +0530

    Add files via upload

---
+++ c/array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[3]={1,2,3};
+int b[10],i;
+printf("enter the values for array b\n");
+for(i=0;i<10;i++)
+{
+b[i]=i;
+}
+printf("value in a[2] %d\n",a[2]);
+printf("values in array b= ");
+for(i=0;i<10;i++)
+printf("%d\n",b[i]);
+}
---
+++ c/myage.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int myage=21;
+printf("my age is:%d\n",myage);
+printf("%p\n",&myage);
+}
+
+
